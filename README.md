0246ab98b07d5af46e79477d2b315e4cb0d9ad02
anastasiialukianenko <121677643+anastasiialukianenko@users.noreply.github.com>
2023-09-23 14:57:41 +0200

    done

---
+++ README.md
@@ -0,0 +1,11 @@
+node index.js --action="list"
+https://ibb.co/9wVCMML
+
+node index.js --action="get" --id 05olLMgyVQdWRwgKfg5J6
+https://ibb.co/D1Wv0S7
+
+node index.js --action="add" --name Mango --email mango@gmail.com --phone 322-22-22
+https://ibb.co/MRy7Mnj
+
+node index.js --action="remove" --id qdggE76Jtbfd9eWJHrssH
+https://ibb.co/RYJtNcb
---
+++ index.js
@@ -0,0 +1,45 @@
+import * as contsctsService from "./contacts.js";
+import { Command } from 'commander';
+
+const program = new Command();
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+          const contactsList = await contsctsService.listContacts();
+          console.log(contactsList);
+      break;
+
+    case 'get':
+          const contact = await contsctsService.getContactById(id);
+          console.log(contact);
+      break;
+
+      case 'add':
+          const newContact = await contsctsService.addContact(name , email, phone );
+          console.log(newContact);
+        break;
+      
+      case 'remove':
+          const deletedContact = await contsctsService.removeContact(id);
+          console.log(deletedContact);
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+invokeAction(argv);
+
